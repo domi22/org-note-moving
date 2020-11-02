d86d28f8d3e483b5dd709a53ebcf27ece2b36c4e
domi <491431567@qq.com>
2020-11-02 18:04:49 +0800

    init-function

---
+++ org-note-common-function/src/main/java/org/note/function/OrgNoteCommonFunctionApp.java
@@ -0,0 +1,12 @@
+package org.note.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgNoteCommonFunctionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgNoteCommonFunctionApp.class,args);
+    }
+}
